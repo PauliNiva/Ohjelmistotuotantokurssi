c1b00d025e41bcb72ba0f37cb081bbde954192b2
Pauli Niva <Pauli@Vulcan.local>
2015-03-27 13:38:09 +0200

    viikko 3, tehtava 5

--- Viikko3/LoginEasyBv1/src/main/java/ohtu/services/AuthenticationService.java
+++ Viikko3/LoginEasyBv1/src/main/java/ohtu/services/AuthenticationService.java
@@ -39,8 +39,18 @@ public class AuthenticationService {
     }
 
     private boolean invalid(String username, String password) {
-        // validity check of username and password
-
+        if (username.length() < 3) {
+            return true;
+        }
+        if (!username.matches("[a-z]+$")) {
+            return true;
+        }
+        if (password.length() < 8) {
+            return true;
+        }
+        if (!password.matches(".*(\\d|\\W).*")) {
+            return true;
+        }
         return false;
     }
 }
